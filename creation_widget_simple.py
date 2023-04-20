de228893b57085203a067a6b1cd4ca849d010819
ameniDraoula <125572570+ameniDraoula@users.noreply.github.com>
2023-04-20 13:12:17 +0200

    Add files via upload

---
+++ creation_widget_simple.py
@@ -0,0 +1,5 @@
+import tkinter
+#creation d'un widget affichant un simple message textuel
+widget=tkinter.Label(None,text="interface graphique simple est crer avec succées!")
+widget.pack() # positionnement d'un label
+widget.mainloop() # lancement de la boucle d'evennement
